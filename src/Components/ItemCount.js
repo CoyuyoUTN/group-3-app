3b462cb44e769b19ee55676995b6e0d6db37e242
Gonzalo <herrero_gonza@hotmail.com>
2022-08-28 18:38:21 -0300

    New change in count

--- src/Components/ItemCount.js
+++ src/Components/ItemCount.js
@@ -8,7 +8,7 @@ const ItemCount = ({ Stock, Initial }) => {
   };
 
   const decrease = () => {
-    count > 0 && setCount(count - 1);
+    count > Initial && setCount(count - 1);
   };
   return (
     <div className="container-button">
